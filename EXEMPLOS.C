3606b2433ed2f4110a351824de78f5fe83e74608
JOSE HENRIQUE <carvalhocostajr@gmail.com>
2025-04-08 01:28:32 +0000

    exercícios

---
+++ EXEMPLOS.C
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+int a = 3, b = 2, c;
+printf("c = a! = b", a,b,c);
---
+++ Exercicio_Introdu_a_programacao.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main(){
+    int num;
+    printf("Entre com um número inteiro: \n");
+    scanf("%d", &num);
+    num = num + num%3;
+    printf("num = %d\n", num);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
